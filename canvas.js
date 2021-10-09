16ce73bc42db1c90e03b96c58e98a20e43cd8ecf
quatricise <stepantrvaj@gmail.com>
2021-10-09 21:12:39 +0200

    changes

--- canvas.js
+++ canvas.js
@@ -1,6 +1,16 @@
 let debug = true;
-
 const PI = Math.PI
+
+let queryInput = document.querySelector('#query-input')
+let labelX = document.querySelector('#real-x-value')
+let labelY = document.querySelector('#real-y-value')
+let sliderX = document.querySelector('#x-value-slider')
+let sliderY = document.querySelector('#y-value-slider')
+let bothAxisCheckbox = document.querySelector('#both-axis-checkbox')
+let selectedObject;
+let queryBox = document.querySelector('#query-box')
+let objects = []; //all complexi-er objects will be there
+
 let canvasBg = document.getElementById('canvas-bg')
 let canvasText = document.getElementById('canvas-text')
 let canvasFg = document.getElementById('canvas-fg')
@@ -19,19 +29,25 @@ canvases.forEach(canvas => {
   canvas.height = window.innerHeight
 })
 
-window.onresize = ()=> {
+window.onresize = () => {
   canvases.forEach(canvas => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
   })
+  cw = window.innerWidth
+  ch = window.innerHeight
 }
 
+let cw = window.innerWidth
+let ch = window.innerHeight
+
 //input related variables
-let mousedown = null
+let mousedown = false
 let mouseStart = null
 
-
-
+let changingXforSelected = false
+let changingYforSelected = false
+let changeBothAxis = false
 let globalTranslate = {
   x: 0,
   y: 0,
@@ -41,9 +57,96 @@ let bgTransMult = 0.5
 let tTransMult = 1
 let fgTransMult = 2
 
+let textColor = 'white'
+let lineHeight = 30
+let font = '16px Arial'
+
+let poem = {
+  stanza1: [
+    `Deep in the shady sadness of a vale`,
+    `Far sunken from the healthy breath of morn,`,
+    `Far from the fiery noon, and eve's one star,`,
+    `Sat gray-hair'd Saturn, quiet as a stone,`,
+  ],
+}
+
+bothAxisCheckbox.addEventListener('change', function() {
+  changeBothAxis = !changeBothAxis
+})
+
 document.addEventListener('keydown', function (e) {
   if(e.code == 'Digit1') debug = !debug
+  if(e.code == 'Backquote') showControls()
 },false)
+
+document.addEventListener('mousemove', function(e) {
+
+  if(selectedObject) {
+    if(changingXforSelected) {
+      let dx = e.clientX - mouseStart.x
+      selectedObject.x += dx
+      labelX.innerHTML = selectedObject.x
+      
+      if(changeBothAxis) {
+        let dy = e.clientY - mouseStart.y
+        selectedObject.y += dy
+        labelY.innerHTML = selectedObject.y  
+      }
+      mouseStart = {
+        x: e.clientX,
+        y: e.clientY,
+      }
+    }
+    if(changingYforSelected) {
+      let dy = e.clientY - mouseStart.y
+      selectedObject.y += dy
+      labelY.innerHTML = selectedObject.y  
+      
+      if(changeBothAxis) {
+        let dx = e.clientX - mouseStart.x
+        selectedObject.x += dx
+        labelX.innerHTML = selectedObject.x
+      }
+      mouseStart = {
+        x: e.clientX,
+        y: e.clientY,
+      }
+    }
+  }
+
+})
+
+
+queryInput.addEventListener('keydown', function (e) {
+  if(e.code == 'Enter' || e.code == 'NumpadEnter') {
+    let filter = objects.filter(obj => obj.id == queryInput.value)
+    if(filter.length == 0) {
+      alert('No object with that id was found.')
+      return
+    }
+    selectedObject = filter[0]
+    labelX.innerHTML = selectedObject.x
+    labelY.innerHTML = selectedObject.y
+    if(selectedObject.x == undefined || selectedObject.y == undefined) console.log('Selected object is missing either X or Y coordinate. pls fix.')
+    console.log(selectedObject)
+  }
+})
+
+sliderX.addEventListener('mousedown', function(e) {
+  changingXforSelected = true
+  mouseStart = {
+    x: e.clientX,
+    y: e.clientY,
+  }
+})
+
+sliderY.addEventListener('mousedown', function(e) {
+  changingYforSelected = true
+  mouseStart = {
+    x: e.clientX,
+    y: e.clientY,
+  }
+})
 
 canvasText.addEventListener('mousedown', function (e) {
   mousedown = true
@@ -56,7 +159,8 @@ canvasText.addEventListener('mousedown', function (e) {
 
 document.addEventListener('mouseup', function (e) {
   mousedown = false
-
+  changingXforSelected = false
+  changingYforSelected = false
 },false)
 
 canvasText.addEventListener('mousemove', function (e) {
@@ -81,13 +185,13 @@ function moveCanvas(e) {
 
 }
 
-
+//main draw
 function draw() {
   clearCtx(bctx,bgTransMult)
   clearCtx(tctx,tTransMult)
   clearCtx(fctx,fgTransMult)
 
-  checkGrid()
+  populateBorderCells()
 
   drawBg(bctx,bgTransMult)
   drawStars(bctx,bgTransMult)
@@ -100,14 +204,45 @@ function draw() {
     // outlineViewport(bctx,bgTransMult)
     // outlineViewport(tctx,tTransMult)
     // outlineViewport(fctx,fgTransMult)
+    gridcells.forEach(cell => {
+      bctx.save()
+      bctx.beginPath()
+      bctx.rect(
+        cell.x * stargrid.cellsize + 1,
+        cell.y * stargrid.cellsize + 1,
+        stargrid.cellsize - 1,
+        stargrid.cellsize - 1
+      )
+      if(cell.marked) bctx.strokeStyle = 'red'
+      else bctx.strokeStyle = 'blue'
+      bctx.lineWidth = 1.5
+      bctx.globalAlpha = 0.3
+      bctx.stroke()
+      if(cell.farcell) {
+        bctx.fillStyle = 'red'
+        bctx.fill()
+      }
+      bctx.closePath()
+      bctx.restore()
+    })
+    fctx.font = '12px arial'
+    fctx.fillStyle = 'white'
+    fctx.fillText(`Viewport offset in grid-cells x: ${vpOffset.x} y: ${vpOffset.y}`, 5 - globalTranslate.x * fgTransMult, 15 - globalTranslate.y * fgTransMult)
+    fctx.fillText(`globalTranslate x:${globalTranslate.x} y:${globalTranslate.y}`, 5 - globalTranslate.x * fgTransMult, 30 - globalTranslate.y * fgTransMult)
+
   }
   
-  drawTextTest(tctx)
+  textObjects.forEach(obj => {
+    obj.draw()
+  })
   
 
   requestAnimationFrame(draw)
   
 }
+//end of main draw
+
+
 //debug functions
 function drawSquare() {
   tctx.fillStyle = 'hsl(224,85%,80%)'
@@ -124,17 +259,15 @@ function drawFgSquare() {
 
 
 
-function drawTextTest(ctx) {
+function drawPoemTest(ctx) {
   ctx.fillStyle = 'hsl(0,0%,95%)'
   ctx.font = '16px Arial'
   ctx.fillText(`Deep in the shady sadness of a vale`,300,300)
-  ctx.fillText(`Far sunken from the healthy breath of morn,`,300,320)
-  ctx.fillText(`Far from the fiery noon, and eve's one star,`,300,340)
-  ctx.fillText(`Sat gray-hair'd Saturn, quiet as a stone,`,300,360)
-
-  ctx.fillText(`globalTranslate x:${globalTranslate.x} y:${globalTranslate.y}`,300,520)
-
+  ctx.fillText(`Far sunken from the healthy breath of morn,`,300,322)
+  ctx.fillText(`Far from the fiery noon, and eve's one star,`,300,344)
+  ctx.fillText(`Sat gray-hair'd Saturn, quiet as a stone,`,300,366)
 }
+
 function clearCtx(ctx,mult) {
   ctx.clearRect(-globalTranslate.x * mult,-globalTranslate.y * mult,window.innerWidth,window.innerHeight)
 }
@@ -155,7 +288,7 @@ function drawBg(ctx,mult) {
 
 let starProperties = {
   density: 20, // per grid cell
-  colors: ['hsl(224,25%,40%)','yellow','orange','magenta'],
+  colors: ['hsl(224,25%,40%)','hsl(55,60%,45%)','hsl(30,80%,40%)','hsl(330,50%,50%)'],
   radius: 1.8,
   radiusRange: 1,
 }
@@ -165,13 +298,17 @@ let stargrid = {
   originX: 0,
   originY: 0,
 }
+
 let stars = [] //the actual array of Star objects which will be rendered
 let gridcells = []
+
 class Gridcell {
   constructor(x,y) {
     this.x = x
     this.y = y
     this.populated = false
+    this.marked = false
+    this.farcell = false
   }
 }
 
@@ -185,6 +322,7 @@ class Star {
       x: Math.floor(this.x / stargrid.cellsize),
       y: Math.floor(this.y / stargrid.cellsize),
     }
+    this.kill = false
   }
   draw(ctx) {
     ctx.moveTo(this.x,this.y)
@@ -218,41 +356,149 @@ function drawStars(ctx,mult) {
   })
   
   ctx.restore()
-}
-//split star drawing into passes based on the number of total possible colors
-
-function checkGrid() {
-  
-}
-
-function initGridcells() {
-  for (let w = 0; w < window.innerWidth / stargrid.cellsize + 1 ; w++) {
-    for (let h = 0; h < window.innerHeight / stargrid.cellsize + 1 ; h++) {
-      gridcells.push(new Gridcell(w,h))
-      
-    }
-  }
-}
-
-function populateCell(cell = {x: 0, y: 0}) {
-
- for (let i = 0; i < starProperties.density; i++) {
-   generateStar(cell)
- }
-
+  fctx.fillText(`Stars rendered: ${starsVisible.length}`, 5 - globalTranslate.x * fgTransMult, 45 - globalTranslate.y * fgTransMult)
+  fctx.fillText(`Stars total: ${stars.length}`, 5 - globalTranslate.x * fgTransMult, 60 - globalTranslate.y * fgTransMult)
 }
 
 function generateStar(cell) {
   let x = cell.x * stargrid.cellsize + Math.floor(Math.random() * stargrid.cellsize)
   let y = cell.y * stargrid.cellsize + Math.floor(Math.random() * stargrid.cellsize)
   let radius = starProperties.radius + (Math.random()*starProperties.radiusRange - starProperties.radiusRange/2)
+  let color = starProperties.colors[Math.floor(Math.random()*starProperties.colors.length + 0.5)]
   stars.push(new Star(
     x,
     y,
     radius,
-    starProperties.colors[0],
+    color,
   ))
 }
+
+
+
+let vpOffset = {x:0,y:0}
+let vpOffsetPrev = vpOffset
+
+function populateBorderCells() {
+  vpOffset = {
+    x: Math.floor(-globalTranslate.x / stargrid.cellsize * bgTransMult),
+    y: Math.floor(-globalTranslate.y / stargrid.cellsize * bgTransMult),
+  }
+  
+  if(vpOffset.x == vpOffsetPrev.x && vpOffset.y == vpOffsetPrev.y) {
+    return
+  }
+  
+  vpOffsetPrev = vpOffset
+  
+  let num = 0
+  for (let x = vpOffset.x - 2; x < cw/stargrid.cellsize + vpOffset.x + 4; x++) {
+    for (let y = vpOffset.y - 2; y < ch/stargrid.cellsize + vpOffset.y + 4; y++) {
+      let add = generateCell(x,y)
+      num += add
+    }
+  }
+  console.log(`Added ${num} new cells to the grid.`)
+  
+
+  let unpopulated = gridcells.filter(
+    cell => 
+    (
+    ( 
+      cell.x <= vpOffset.x + 1 && 
+      cell.x >= vpOffset.x - 2
+    )
+    ||
+    (
+      cell.x >= vpOffset.x + cw/stargrid.cellsize - 1 &&
+      cell.x <= vpOffset.x + cw/stargrid.cellsize + 2
+    )
+    ||
+    ( 
+      cell.y <= vpOffset.y + 1 && 
+      cell.y >= vpOffset.y - 2
+    )
+    ||
+    (
+      cell.y >= vpOffset.y + ch/stargrid.cellsize - 1 &&
+      cell.y <= vpOffset.y + ch/stargrid.cellsize + 2
+    )
+    )
+    &&
+    cell.populated == false
+  )
+
+  unpopulated.forEach(cell => {
+    populateCell(cell)
+  })
+  if(debug) console.log(`Cells populated: ${unpopulated.length}`)
+}
+
+
+
+
+function generateCell(x,y) {
+  let filtered = gridcells.filter(
+    cell => 
+    cell.x == x && 
+    cell.y == y
+  )
+
+  if(filtered.length == 0) {
+    gridcells.push(new Gridcell(x,y))
+    return 1
+  } else {
+    return 0
+  }
+
+}
+
+
+function initGrid() {
+  for (let x = -2; x < window.innerWidth / stargrid.cellsize + 2 ; x++) {
+    for (let y = -2; y < window.innerHeight / stargrid.cellsize + 2 ; y++) {
+      gridcells.push(new Gridcell(x,y))
+    }
+  }
+  let visible = gridcells.filter(cell => cell.x >= vpOffset.x && cell.x <= cw/stargrid.cellsize + 1)
+  visible.forEach(cell=> {
+    populateCell(cell)
+  })
+}
+
+function populateCell(cell = null) {
+  if(cell == null || cell.populated) return
+  for (let i = 0; i < starProperties.density; i++) {
+    generateStar(cell)
+  }
+  cell.populated = true
+
+}
+
+
+class TextObject {
+  constructor(x,y,text, id = Math.floor(Math.random()*1_000_000_000)) {
+    this.x = x
+    this.y = y
+    this.text = text
+    // this.id = Math.floor(Math.random()*1_000_000_000)
+    this.id = id
+    objects.push(this)
+  }
+  draw() {
+    tctx.fillStyle = textColor
+    tctx.font = font
+    for (let i = 0; i < this.text.length; i++) {
+      tctx.fillText(this.text[i],this.x,this.y + lineHeight*i - lineHeight)
+      
+    }
+  }
+}
+
+let textObjects = []
+let poemState = 0
+textObjects.push(new TextObject(100,200,poem['stanza1'],'tom'))
+textObjects.push(new TextObject(100,200,poem['stanza1'],'jerry'))
+poemState++
 
 
 function loadAsset(asset) {
@@ -263,11 +509,10 @@ function loadAsset(asset) {
   }
 }
 
-initGridcells()
+function showControls() {
+  queryBox.classList.toggle('hidden')
+}
 
-//issue nastiness and unclean garbage code
-gridcells.forEach(cell => {
-  populateCell(cell)
-})
-populateCell()
+initGrid()
+
 draw()
